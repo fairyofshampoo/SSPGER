8f567fb1be9c4d1ede784f68cb4901751c8aba8e
fairyofshampoo <michellemoreno1313@gmail.com>
2023-04-08 16:38:32 -0600

    commit xd

--- SPGER/src/mx/uv/fei/sspger/logic/Anteproyecto.java
+++ SPGER/src/mx/uv/fei/sspger/logic/Anteproyecto.java
@@ -2,13 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package spger;
+package mx.uv.fei.sspger.logic;
 
 /**
  *
  * @author miche
  */
-public class SPGER {
+public class Anteproyecto {
 
     /**
      * @param args the command line arguments
