cc34465df1df33b2bc3b86b081a2b48b3ccc497d
mario <mario@DESKTOP-CGUI3D0>
2023-04-11 14:01:37 -0600

    Crear conexión a la base de datos

--- SPGER/src/mx/uv/fei/sspger/dataaccess/DataBaseManager.java
+++ SPGER/src/mx/uv/fei/sspger/dataaccess/DataBaseManager.java
@@ -1,14 +1,46 @@
 package mx.uv.fei.sspger.dataaccess;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Logger;
+import java.util.logging.Level;
 
-/**
- *
- * @author miche
- */
-public class DataBaseManager {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+public class DataBaseManager{
+    private Connection connection;
+    private final String DATABASE_NAME = "spger-database-2023.mysql.database.azure.com";
+    private final String DATABASE_USER = "ikariscraft";
+    private final String DATABASE_PASSWORD = "Mario_Nara_Mich";
+
+    public DataBaseManager() {
+    }
+    
+    public Connection getConnection() throws SQLException{
+        connect();
+        return connection;
+    }
+    
+    private void connect() throws SQLException{
+        connection = DriverManager.getConnection(DATABASE_NAME, DATABASE_USER, DATABASE_PASSWORD);
+    }
+    
+    public void closeConnection(){
+        if(connection != null){
+            try{
+                if(!connection.isClosed()){
+                    connection.close();
+                }
+            } catch(SQLException exception){
+                Logger.getLogger(DataBaseManager.class.getName()).log(Level.SEVERE, null, exception);
+            }
+        }
+    }
     
 }
