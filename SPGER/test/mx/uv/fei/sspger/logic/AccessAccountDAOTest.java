7b64fafcf26f112108a1576b44e8824f9d0e3377
fairyofshampoo <michellemoreno1313@gmail.com>
2023-04-14 18:59:24 -0600

    Test unit

---
+++ SPGER/test/mx/uv/fei/sspger/logic/AccessAccountDAOTest.java
@@ -0,0 +1,82 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package mx.uv.fei.sspger.logic;
+
+import java.util.List;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import java.sql.SQLException;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author miche
+ */
+public class AccessAccountDAOTest {
+    
+    public AccessAccountDAOTest() {
+    }
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @AfterEach
+    public void tearDown() {
+    }
+
+    /**
+     * Test of addAccessAccount method, of class AccessAccountDAO.
+     */
+    @Test
+    public void testAddAccessAccount() throws SQLException {
+        System.out.println("addAccessAccount");
+        AccessAccount accessAccount = new AccessAccount();
+        accessAccount.setInstitutionalEMail("oalonso@uv.mx");
+        accessAccount.setPassword("Miri0301");
+        accessAccount.setPrivileges(1);
+        AccessAccountDAO instance = new AccessAccountDAO();
+        int expResult = 0;
+        int result = instance.addAccessAccount(accessAccount);
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of getAccessAccount method, of class AccessAccountDAO.
+     */
+    @Test
+    public void testGetAccessAccount() throws SQLException {
+        System.out.println("getAccessAccount");
+        String email = "";
+        AccessAccountDAO instance = new AccessAccountDAO();
+        AccessAccount expResult = null;
+        AccessAccount result = instance.getAccessAccount(email);
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of getAllAccessAccounts method, of class AccessAccountDAO.
+     */
+    @Test
+    public void testGetAllAccessAccounts() throws Exception {
+        System.out.println("getAllAccessAccounts");
+        AccessAccountDAO instance = new AccessAccountDAO();
+        List<AccessAccount> expResult = null;
+        List<AccessAccount> result = instance.getAllAccessAccounts();
+        assertEquals(expResult, result);
+    }
+    
+}
---
+++ SPGER/test/mx/uv/fei/sspger/logic/ProjectDAOTest.java
@@ -0,0 +1,78 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package mx.uv.fei.sspger.logic;
+
+import java.util.List;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author miche
+ */
+public class ProjectDAOTest {
+    
+    public ProjectDAOTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of addProject method, of class ProjectDAO.
+     */
+    @Test
+    public void testAddProject() throws SQLException {
+        System.out.println("addProject");
+        Project project = new Project();
+        project.setName("Nombre1");
+        project.setDescription("Descripcion ejemplo");
+        project.setExpectedResults("Resultados esperados ejemplo");
+        project.setDuration(12);
+        project.setNotes("Notas ejemplo");
+        project.setRequeriments("Requisitos ejemplo");
+        project.setBibliography("Bibliografia ejemplo");
+        String idCuerpoAcademico = "1";
+        String idLgac = "L2";
+        
+        ProjectDAO instance = new ProjectDAO();
+        
+        int expectedResult = 1;
+        
+        int result = instance.addProject(project, idCuerpoAcademico, idLgac);
+        assertEquals(expectedResult, result);
+    }
+
+    /**
+     * Test of getAllProjects method, of class ProjectDAO.
+     */
+    @Test
+    public void testGetAllProjects() throws SQLException {
+        System.out.println("getAllProjects");
+        ProjectDAO instance = new ProjectDAO();
+        List<Project> expResult = null;
+        List<Project> result = instance.getAllProjects();
+        assertEquals(expResult, result);
+    }
+    
+}
